df58d6d325e0749e47b5d45c49680e5ea64666a3
Jeremy <jeremy.teurterie@gmail.com>
2023-07-14 09:16:29 +0700

    :sparkles: Feat: creating routes using express router

--- server.js
+++ server.js
@@ -1,10 +1,16 @@
 const express = require('express');
 const dotenv = require('dotenv');
 
+// route files
+const bootcamps = require('./routes/bootcamps');
+
 // load env vars
 dotenv.config({ path: './config/config.env' });
 
 const app = express();
+
+// mount router
+app.use('/api/v1/bootcamps', bootcamps);
 
 const PORT = process.env.PORT || 4000;
 
