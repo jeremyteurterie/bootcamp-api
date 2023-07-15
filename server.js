1d5b4031a9179b1303f6d37c116f51d1826c8de6
Jeremy <jeremy.teurterie@gmail.com>
2023-07-15 07:47:51 +0700

    :sparkles: Add morgan & middleware

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const dotenv = require('dotenv');
+const morgan = require('morgan');
 
 // route files
 const bootcamps = require('./routes/bootcamps');
@@ -9,6 +10,10 @@ dotenv.config({ path: './config/config.env' });
 
 const app = express();
 
+// dev logging middleware
+if (process.env.NODE_ENV === 'development') {
+  app.use(morgan('dev'));
+}
 // mount router
 app.use('/api/v1/bootcamps', bootcamps);
 
