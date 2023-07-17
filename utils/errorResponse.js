0860f013a4b80be60af88603c9d38d04778bcbef
Jeremy <jeremy.teurterie@gmail.com>
2023-07-17 10:09:29 +0700

    :sparkles: Feat: custom errorresponse class

--- utils/errorResponse.js
+++ utils/errorResponse.js
@@ -1,8 +1,13 @@
+// Définition de la classe ErrorResponse
 class ErrorResponse extends Error {
   constructor(message, statusCode) {
+    // Appel du constructeur de la classe parent "Error" avec le message d'erreur
     super(message);
+
+    // Définition d'une propriété "statusCode" spécifique pour cette classe ErrorResponse
     this.statusCode = statusCode;
   }
 }
 
+// Exportation de la classe ErrorResponse pour pouvoir l'utiliser ailleurs dans votre code
 module.exports = ErrorResponse;
