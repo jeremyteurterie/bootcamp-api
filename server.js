a0c9a4ddde5c93b670756edd7d4b57efcb33641b
Jeremy <jeremy.teurterie@gmail.com>
2023-08-01 09:23:44 +0700

    :sparkles: Feat: user model

--- server.js
+++ server.js
@@ -16,6 +16,7 @@ connectDB();
 // route files
 const bootcamps = require('./routes/bootcamps');
 const courses = require('./routes/courses');
+const auth = require('./routes/auth');
 const { connect } = require('mongoose');
 
 const app = express();
@@ -37,6 +38,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 // mount router
 app.use('/api/v1/bootcamps', bootcamps);
 app.use('/api/v1/courses', courses);
+app.use('/api/v1/auth', auth);
 
 app.use(errorHandler);
 
